cef39d90ffe22a34c374c43aa1451749f853620b
Wilson Parson <wilson.parson@atomicfi.com>
2023-03-21 22:58:34 -0600

    fix: only load animation on home page

--- functions.php
+++ functions.php
@@ -4,7 +4,9 @@ function enqueue_scripts()
 {
 
     wp_enqueue_style('global-style', get_template_directory_uri() . '/css/styles.css');
-    wp_enqueue_script('animate-headline-suffix', get_template_directory_uri() . '/js/animate-headline-suffix.js');
+    if (is_front_page()) {
+        wp_enqueue_script('animate-headline-suffix', get_template_directory_uri() . '/js/animate-headline-suffix.js');
+    }
 }
 
 add_action('wp_enqueue_scripts', 'enqueue_scripts');
