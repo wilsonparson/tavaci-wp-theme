d8d77560b35073af9772833a64ea66a37e93ae59
Wilson Parson <wilson.parson@atomicfi.com>
2023-05-01 01:06:06 -0600

    feat: finish 2023 concert program

--- single-concert-program.php
+++ single-concert-program.php
@@ -41,99 +41,137 @@ function participant_as_html($participant)
 
     if ($participant->type == 'list') {
         $names_as_list_items = names_as_list_items($participant->names);
+        $list = "<ul>$names_as_list_items</ul>";
 
-        return "
-        <dl>
-            <dt class=\"participant-title\">$participant->title</dt>
-            <dd>
-                <ul>
-                    $names_as_list_items
-                </ul>
-            </dd>
-        </dl>
-        ";
+        if (isset($participant->title)) {
+            return "
+                <h4 class=\"participant-title\">$participant->title</h5>
+                $list
+            ";
+        }
+
+        return $list;
     }
 
-    if ($participant->type == 'multi-column-list') {
-        $names_as_list_items = names_as_list_items($participant->names);
+    if ($participant->type == 'collapsible') {
+        $content = $participant->content;
+        $content_as_html = participant_as_html($content);
 
-        return "<ul class=\"multi-column-list\">$names_as_list_items</ul>";
+        return "
+            <details>
+                <summary>
+                $participant->title (click to expand)
+                </summary>
+                $content_as_html
+            </details>
+        ";
     }
 }
 ?>
 
 <main id="single-concert-program">
-    <h1><?= $title; ?></h1>
+    <div class="g-max-width-wrapper">
 
-    <nav>
-        <ul>
-            <li><a href="#program">Program</a></li>
-            <li><a href="#students">Students</a></li>
-            <li><a href="#teachers">Teachers</a></li>
-        </ul>
-    </nav>
+        <h1><?= $title; ?></h1>
 
-    <section id="program">
-        <h2>Program</h2>
-        <table>
-            <tbody>
-                <?php foreach ($program->songs as $song) : ?>
-                    <tr>
-                        <th><?= $song->title ?></th>
-                        <td>
-                            <?php foreach ($song->participants as $participant) : ?>
-                                <?php echo participant_as_html($participant); ?>
-                            <?php endforeach; ?>
-                        </td>
-                    </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
-    </section>
+        <nav>
+            <ul>
+                <li><a href="#program">Program</a></li>
+                <li><a href="#students">Students</a></li>
+                <li><a href="#teachers">Teachers</a></li>
+            </ul>
+        </nav>
 
-    <section id="students">
-        <h2>Students</h2>
-    </section>
-
-    <section id="teachers">
-        <h2>Teachers</h2>
-    </section>
-
-    <section id="students">
-        <h2>Students</h2>
-
-        <section id="prek-k">
-            <h3>Pre-K – K</h3>
-            <?= students_as_ul('prek-k.txt'); ?>
+        <section id="program">
+            <h2>Program</h2>
+            <table>
+                <tbody>
+                    <?php foreach ($program->songs as $song) : ?>
+                        <tr>
+                            <th><?= $song->title ?></th>
+                            <td>
+                                <?php foreach ($song->participants as $participant) : ?>
+                                    <?php echo participant_as_html($participant); ?>
+                                <?php endforeach; ?>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
         </section>
 
-        <section id="1st-2nd-grade">
-            <h3>1st – 2nd Grade</h3>
-            <?= students_as_ul('1st-2nd-grade.txt'); ?>
+
+        <section id="students">
+            <h2>Students</h2>
+
+            <section id="prek-k" class="g-well">
+                <h3>Pre-K – K</h3>
+                <?= students_as_ul('prek-k.txt'); ?>
+            </section>
+
+            <section id="1st-2nd-grade" class="g-well">
+                <h3>1st – 2nd Grade</h3>
+                <?= students_as_ul('1st-2nd-grade.txt'); ?>
+            </section>
+
+            <section id="3rd-7th-grade" class="g-well">
+                <h3>3rd – 7th Grade</h3>
+                <?= students_as_ul('3rd-7th-grade.txt'); ?>
+            </section>
+
         </section>
 
-        <section id="3rd-7th-grade">
-            <h3>3rd – 7th Grade</h3>
-            <?= students_as_ul('3rd-7th-grade.txt'); ?>
+        <section id="teachers">
+            <h2>Teachers</h2>
+            <div class="g-well">
+                <figure>
+                    <img src="/wp-content/themes/tavaci-wp-theme/img/avatar-megan.jpeg" alt="" />
+                    <figcaption>Megan Owen</figcaption>
+                </figure>
+                <figure>
+                    <img src="/wp-content/themes/tavaci-wp-theme/img/avatar-karalee.jpeg" alt="" />
+                    <figcaption>Karalee Kidd</figcaption>
+                </figure>
+                <div class="teacher-lists">
+                    <div class="teacher-list">
+                        <h3>Assistant Teachers</h3>
+                        <ul>
+                            <li>Sherrie Wayment</li>
+                            <li>Vali Veater</li>
+                        </ul>
+                    </div>
+                    <div class="teacher-list">
+                        <h3>Student Teachers</h3>
+                        <ul>
+                            <li>Brooke Walker</li>
+                            <li>Lauren Brockbank</li>
+                            <li>Lydia Donaldson</li>
+                            <li>Ethan McArthur</li>
+                            <li>Madelyn Little</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </section>
 
-    </section>
+        <section id="special-thanks">
+            <h2>Special Thanks</h2>
+            <div class="g-well">
 
-    <section id="special-thanks">
-        <h2>Special Thanks</h2>
-        <ul>
-            <li>
-                <strong>TavaCi Corp., Tami Van Dusen, and Cindy Bowen</strong> for
-                providing this amazing program
-            </li>
-            <li>
-                <strong>Melanie Day</strong> and the
-                <strong>Box Elder High School Tech and Light Crew</strong>
-            </li>
-            <li><strong>Candice Nichols</strong>, Backstage Manager</li>
-        </ul>
-    </section>
-
+                <ul>
+                    <li>
+                        <strong>TavaCi Corp., Tami Van Dusen, and Cindy Bowen</strong> for
+                        providing this amazing program
+                    </li>
+                    <li>
+                        <strong>Melanie Day</strong> and the
+                        <strong>Box Elder High School Tech and Light Crew</strong>
+                    </li>
+                    <li><strong>Candice Nichols</strong>, Backstage Manager</li>
+                </ul>
+            </div>
+        </section>
+    </div>
 </main>
 
 <?php get_footer(); ?>
